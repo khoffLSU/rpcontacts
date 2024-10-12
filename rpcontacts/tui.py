b60e50c117580f54b12a24104a1f8cb2c8bc6b14
Keith Hoffman <keithnhoffman@gmail.com>
2024-10-12 16:17:55 -0500

    Initial commit with basic skeleton

---
+++ rpcontacts/tui.py
@@ -0,0 +1,12 @@
+from textual.app import App
+from textual.widgets import Header, Footer
+
+class ContactsApp(App):
+    def compose(self):
+        yield Header
+        yield Footer
+    
+    def on_mount(self):
+        self.title = "RP Contacts"
+        self.sub_title = "A Contacts book with Textual & Python"
+
