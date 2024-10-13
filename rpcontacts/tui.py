796ff5ebd54f768763f2629c2ca76f6289124f8a
Keith Hoffman <keithnhoffman@gmail.com>
2024-10-12 21:33:15 -0500

    Add quit dialog class

--- rpcontacts/tui.py
+++ rpcontacts/tui.py
@@ -1,5 +1,7 @@
 from textual.app import App
-from textual.widgets import Header, Footer
+from textual.containers import Grid
+from textual.screen import Screen
+from textual.widgets import Button, Footer, Header, Label
 
 class ContactsApp(App):
     BINDINGS = [
@@ -16,3 +18,25 @@ class ContactsApp(App):
 
     def action_toggle_dark(self):
         self.dark = not self.dark
+
+class QuestionDialog(Screen):
+    def __init__(self, message, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.message = message
+
+    def compose(self):
+        no_button = Button("No", variant="primary", id="No")
+        no_button.focus()
+
+        yield Grid(
+            Label(self.message, id = "question"),
+            Button("Yes", variant="error", id="yes"),
+            no_button,
+            id="question-dialog"
+        )
+    
+    def on_button_pressed(self, event):
+        if event.button.id == "yes":
+            self.dismiss(True)
+        else:
+            self.dismiss(False)
