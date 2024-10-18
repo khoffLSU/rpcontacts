9cc4f689237528d0dbff4222a9c8fe301a5feac3
Keith Hoffman <keithnhoffman@gmail.com>
2024-10-18 11:01:04 -0500

    Add database call on load of main

--- rpcontacts/__main__.py
+++ rpcontacts/__main__.py
@@ -1,7 +1,8 @@
+from rpcontacts.database import Database
 from rpcontacts.tui import ContactsApp
 
 def main():
-    app = ContactsApp()
+    app = ContactsApp(db=Database())
     app.run()
 
 if __name__ == "__main__":
